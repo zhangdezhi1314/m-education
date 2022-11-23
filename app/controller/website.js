1babf5301b1c6cb6d3e1bc5c8fa9d80c6f75b0ff
dezhizhang <1541609448@qq.com>
2022-11-23 13:36:59 +0800

    feat:新增删除

--- app/controller/website.js
+++ app/controller/website.js
@@ -5,7 +5,7 @@
  * :copyright: (c) 2022, Tungee
  * :date created: 2022-11-23 11:09:53
  * :last editor: 张德志
- * :date last edited: 2022-11-23 11:26:39
+ * :date last edited: 2022-11-23 12:31:41
  */
 'use strict';
 
@@ -26,12 +26,26 @@ class HomeController extends Controller {
   // 获取所有网站列表
   async list() {
     const ctx = this.ctx;
-    const result = await ctx.model.Website.find();
+    const body = ctx.request.body;
+    const result = await ctx.model.Website.find(body);
     ctx.body = {
       code: 200,
       msg: '获取网站成功',
       success: true,
       data: result,
+    };
+  }
+
+  // 删除网站
+  async delete() {
+    const ctx = this.ctx;
+    const body = ctx.request.body;
+    await ctx.model.Website.deleteOne(body);
+    ctx.body = {
+      code: 200,
+      msg: '删除成功',
+      success: true,
+      data: null,
     };
   }
 
