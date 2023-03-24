e284f757792ba45f94c6072b526cbef6453cd7c0
dezhizhang <1541609448@qq.com>
2023-03-24 09:19:57 +0800

    feat:update

--- app/model/website.js
+++ app/model/website.js
@@ -1,13 +1,4 @@
 /*
- * :file description: 
- * :name: /m-xiaozhicloud/app/model/website.js
- * :author: 张德志
- * :copyright: (c) 2022, Tungee
- * :date created: 2022-11-23 11:12:24
- * :last editor: 张德志
- * :date last edited: 2022-11-23 11:16:53
- */
-/*
  * :file description:
  * :name: /m-xiaozhicloud/app/model/manager.js
  * :author: 张德志
